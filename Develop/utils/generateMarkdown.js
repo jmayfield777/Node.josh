93155955c14d3d69d16fa5b65f8a42422ccab22d
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2024-01-24 13:59:28 -0500

    added renderLicenseBadge function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = '';
+
+  if (license != 'None') {
+    badge = `![License Badge](https://img.shields.io/badge/license-${license}-blue)`;
+  }
+
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
