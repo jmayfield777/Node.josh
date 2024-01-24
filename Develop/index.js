62793c3a0cdb96308c61cd1688344e45648df6da
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2024-01-24 13:46:20 -0500

    added questions to questions array

--- Develop/index.js
+++ Develop/index.js
@@ -11,7 +11,54 @@ const licenses = [
 ];
 
 // TODO: Create an array of questions for user input
-const questions = []
+const questions = [
+    {
+      type: 'input',
+      name: 'username',
+      message: 'What is your Github username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the project name?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please write a short description of your project.',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What kind of license should your project have?',
+      choices: licenses,
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What command should be run to install dependencies?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What command should be run to run tests?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What does the user need to know about using the repo?',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'What does the user need to know about contributing to the repo?',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
