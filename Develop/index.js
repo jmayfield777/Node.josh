e1f863be7fd48b2d434d3317c1b34e1f0ff9f311
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2024-01-24 13:49:49 -0500

    added function to write README file

--- Develop/index.js
+++ Develop/index.js
@@ -61,7 +61,12 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.log(err) : console.log('Successfully created README.md!');
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
